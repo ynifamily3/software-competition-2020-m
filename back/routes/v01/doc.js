ccaa39f1424ca221f2050bb8e29fecbeae7fb9f8
TaliSm4N <evenine9@gmail.com>
2020-01-19 16:16:47 +0000

    create 명령 info/attr에 대해서 추가

--- back/routes/v01/doc.js
+++ back/routes/v01/doc.js
@@ -1,25 +1,6 @@
 const router = require('express').Router();
 const docManager = require('./docManager')
 
-router.post('/createSubject',(req,res,next)=>{
-
-	console.log("post")
-
-	console.log(`name ${req.body.name}`);
-	console.log(`id ${req.body.parentId}`);
-
-	docManager.makeSubject(res,req.body.name);
-
-	/*
-	return res.json({
-    state : "Boolean",
-    msg   : "String",
-    id    : "MongoDBId"
-	});
-	*/
-
-});
-
 router.get('/getInfosList',(req,res,next)=>{
 
 	return res.json({state:'Boolean',msg:'string',names:['String'],ids:['MongoDBId']});
@@ -27,24 +8,33 @@ router.get('/getInfosList',(req,res,next)=>{
 
 router.post('/createInfo',(req,res,next)=>{
 
-	console.log(`name ${req.body.name}`);
-	console.log(`id ${req.body.parentId}`);
+	
+	//if(!req.body.parentId)
+	//	return docManager.makeInfo(res,req.body.name,null)
+	//else
+		return docManager.makeInfo(res,req.body.name,req.body.parentId);
 
-	return res.json({
-    state : "Boolean",
-    msg   : "String",
-    id    : "MongoDBId"
-	});
+	//return res.json({
+	//    state : "Boolean",
+	//    msg   : "String",
+	//    id    : "MongoDBId"
+	//	});
 });
 
-router.post('/modifyInfo',(req,res,next)=>{
-	console.log(`name ${req.body.name}`);
-	console.log(`id ${req.body.parentId}`);
+router.post('/createAttr',(req,res,next)=>{
+	return docManager.makeAttr(res,req.body.prefix,req.body.content,req.body.postfix,req.body.parentId);
+})
 
-	return res.json({
-		state:"Boolean",
-		msg:"String"
-	})
+router.post('/modifyInfo',(req,res,next)=>{
+	//	console.log(`name ${req.body.name}`);
+	//console.log(`id ${req.body.parentId}`);
+	//
+	return docManage.modifyInfo(res,req.body.name,req.body.id);
+
+	//	return res.json({
+	//		state:"Boolean",
+	//	msg:"String"
+	//	})
 });
 
 router.delete('/deleteInfo',(req,res,next)=>{
--- back/routes/v01/docManager.js
+++ back/routes/v01/docManager.js
@@ -1,41 +1,133 @@
 const Attr = require('../../models/attr');
 const Info = require('../../models/info');
-const Subject = require('../../models/subject');
 
-exports.makeSubject = (res,subjectName) => {
-	Subject.findOne({name:subjectName})
-		.then((sub)=>{
-			if(!sub){
-				console.log('make subject');
-				let newSubject = new Subject({
-					name:subjectName,
-					attrs:[],
-					childe:[]
-				})
-				newSubject.save()
-					.then((subject)=>{
-						return res.json({
-							state:true,
-							msg:'Success',
-							id:subject._id
-						})
+exports.makeInfo = (res,infoName,parent) => {
+
+	if(parent==null||parent==undefined)
+	{
+		Info.findOne({name:infoName})
+			.then((info)=>{
+				if(!info){
+					let newInfo=new Info({
+						name:infoName,
+						attrs:[],
+						childs:[]
 					})
-			}
-			else {
-				console.log('makeSubject failed')
+					newInfo.save()
+						.then((info)=>{
+							return res.json({
+								state:true,
+								msg:'Success',
+								id:info._id
+							})
+						})
+				}
+				else{
+					return res.json({
+						state:false,
+						msg:'Same InfoName',
+						id:null
+					})
+				}
+			})
+			.catch((err)=>{
 				return res.json({
 					state:false,
-					msg:'Same SubjectName',
+					msg:'InfoName failed',
 					id:null
 				})
-			}
+			})
+	} else {
+		Info.findOne({_id:parent}).populate('childs')
+			.then(async (parent)=>{
+				if(!parent){
+					return res.json({
+						state:false,
+						msg:'Parent Not found',
+						id:null
+					})
+				}
+				else
+				{
+					for(child of parent.childs){
+						if(child.name==infoName){
+							return res.json({
+								state:false,
+								msg:'Same infoName',
+								id:null
+							})
+						}
+					}
+					let newInfo= new Info({
+						name:infoName,
+						attrs:[],
+						childs:[],
+					})
+					await newInfo.save()
+						.then(async (info)=>{
+							await Info.findOneAndUpdate({_id:parent._id},{$push:{childs:info}})
+
+							return res.json({
+								state:true,
+								msg:'Success',
+								id:newInfo._id
+							})
+						})
+						.catch((err)=>{
+							return res.json({
+								state:false,
+								msg:'Save DB error',
+								id:null
+							})
+						})
+				}
+			})
+			.catch((err)=>{
+				return res.json({
+					state:false,
+					msg:'ParentId error',
+					id:null
+				})
+			})
+	}
+
+}
+
+exports.makeAttr = (res,prefix,content,postfix,parentId)=>{
+
+	Info.findOne({_id:parentId})
+		.then((info)=>{
+			let newAttr = new Attr({
+				prefix,
+				content,
+				postfix,
+				parentId:info._id
+			})
+			newAttr.save()
+				.then(async (attr)=>{
+					await Info.findOneAndUpdate({_id:info._id},{$push:{attrs:attr}})
+
+					return res.json({
+						state:true,
+						msg:'Success',
+						aid:attr._id
+					})
+				})
 		})
 		.catch((err)=>{
-			console.log('makeSubject failed d error')
-				return res.json({
-					state:false,
-					msg:'Same SubjectName',
-					id:null
-				})
+			return res.json({
+				state:false,
+				msg:'Info Not Found',
+				id:null
+			})
 		})
-};
+}
+
+exports.modifyInfo = (res,name,id)=>{
+
+	Info.findOne({_id:id})
+		.then((info)=>{
+
+		})
+
+}
