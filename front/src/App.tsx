80b95ca3b7f583ee034d889911975b8e83fa4b0d
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-01-26 22:16:07 +0900

    modal 추가, 과목간 이동, 추가 기능 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { MyLocalModelContextProvider } from './contexts/MyLocalModel';
-import { ModalProvider } from 'react-modal-hook';
+// import { ModalProvider } from 'react-modal-hook';
 import './App.scss';
 import AppIn from './AppIn';
 
 function App() {
   return (
     <MyLocalModelContextProvider>
-      <ModalProvider>
-        <AppIn />
-      </ModalProvider>
+      <AppIn />
     </MyLocalModelContextProvider>
   );
 }
--- front/src/components/AttrModal.tsx
+++ front/src/components/AttrModal.tsx
@@ -1,16 +1,65 @@
 import React from 'react';
-import ReactModal from 'react-modal';
-import { useModal } from 'react-modal-hook';
+import Modal from 'react-modal';
+// import { useModal } from 'react-modal-hook';
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+  },
+  content: {
+    border: '1px solid black',
+    borderRadius: 0,
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
 
 function AttrModal() {
-  // https://www.npmjs.com/package/react-modal
-  const [showModal, hideModal] = useModal(() => (
-    <ReactModal isOpen>
-      <p>Modal content</p>
-      <button onClick={hideModal}>Hide</button>
-    </ReactModal>
-  ));
-  return <button onClick={showModal}>Attr 추가</button>;
+  var subtitle: any;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        shouldCloseOnOverlayClick={false}
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </React.Fragment>
+  );
 }
 
 export default AttrModal;
